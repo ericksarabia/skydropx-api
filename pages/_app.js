cb40cff6babc743a9e6e382945126a5a71f1226d
Erick Sarabia <ecksarabia@gmail.com>
2021-02-27 13:33:51 -0600

    Add styled components config

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import "../styles/globals.css";
+import App from "next/app";
+import GlobalStyles from "../styles/globals";
 
-function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+class MyApp extends App {
+	render() {
+		const { Component, pageProps } = this.props;
+		return (
+			<>
+				<Component {...pageProps} />
+				<GlobalStyles />
+			</>
+		);
+	}
 }
 
 export default MyApp;
