e7881344c76e5a3db9338dd4398a97b3b6342097
Erick Sarabia <ecksarabia@gmail.com>
2021-02-28 01:08:08 -0600

    Fix eslint warnings

--- components/Form/Form.js
+++ components/Form/Form.js
@@ -1,19 +1,17 @@
 import { Wrapper, Image, Input, Button, Label, Group, Message } from "./styled";
 
-const Form = ({ handleClick, inputs, message }) => {
-	return (
-		<Wrapper>
-			<Image alt="skydropx" src="/logo.svg" />
-			{inputs.map((input) => (
-				<Group>
-					{input.label && <Label>{input.label}</Label>}
-					<Input type={input.type} placeholder={input.placeholder} id={input.id} />
-				</Group>
-			))}
-			{message && <Message>Todos los campos son obligatorios</Message>}
-			<Button onClick={handleClick}>Aceptar</Button>
-		</Wrapper>
-	);
-};
+const Form = ({ handleClick, inputs, message }) => (
+	<Wrapper>
+		<Image alt="skydropx" src="/logo.svg" />
+		{inputs.map((input) => (
+			<Group>
+				{input.label && <Label>{input.label}</Label>}
+				<Input type={input.type} placeholder={input.placeholder} id={input.id} />
+			</Group>
+		))}
+		{message && <Message>Todos los campos son obligatorios</Message>}
+		<Button onClick={handleClick}>Aceptar</Button>
+	</Wrapper>
+);
 
 export default Form;
