e96788db1a553d7a928e2c2e8d84d476f1515a23
Erick Sarabia <ecksarabia@gmail.com>
2021-03-01 17:25:31 -0600

    Add Anchor component

---
+++ components/Anchor/Anchor.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Anchor = styled.a`
+	width: 100%;
+	font-size: 1.5rem;
+	margin-top: 2rem;
+	color: #000;
+	text-decoration: underline;
+	cursor: pointer;
+`;
+
+export default Anchor;
