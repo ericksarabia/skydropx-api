3b3d7449bdce64f1b66399c8b0e13b148d610e9b
Erick Sarabia <ecksarabia@gmail.com>
2021-03-01 04:15:36 -0600

    Add dynamic shipment page

---
+++ pages/shipment/[id].js
@@ -0,0 +1,42 @@
+import Container from "../../components/Container";
+import Logo from "../../components/Logo";
+import Card from "../../components/Card";
+
+const Shipment = ({ rates }) => (
+	<Container>
+		<Logo alt="skydropx" src="/logo.svg" />
+		<ul>
+			{rates.map((rate) => (
+				<li>
+					<Card rate={rate} />
+				</li>
+			))}
+		</ul>
+	</Container>
+);
+
+export const getServerSideProps = async ({ query }) => {
+	const RESPONSE = await fetch(`https://api-demo.skydropx.com/v1/shipments/${query.id}`, {
+		headers: {
+			"Content-Type": "application/json",
+			Authorization: `Token token=${process.env.SKYDROPX_TOKEN}`,
+		},
+	});
+	const SHIPMENT = await RESPONSE.json();
+	let rates = SHIPMENT.included.filter((item) => item.type === "rates");
+	rates = rates.map(({ attributes }) => ({
+		...attributes,
+		average: attributes.days * Number(attributes.total_pricing),
+		best: false,
+	}));
+	rates.sort((a, b) => (a.average > b.average ? 1 : -1));
+	rates[0].best = true;
+
+	return {
+		props: {
+			rates,
+		},
+	};
+};
+
+export default Shipment;
