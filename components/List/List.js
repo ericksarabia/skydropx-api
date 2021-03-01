daf4646fde6b2d46359bd37b18646fa052821bc5
Erick Sarabia <ecksarabia@gmail.com>
2021-03-01 14:19:16 -0600

    Add list component to display rate services

---
+++ components/List/List.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+const List = styled.ul`
+	display: flex;
+	width: 85%;
+	flex-wrap: wrap;
+	justify-content: flex-start;
+	align-items: center;
+	padding: 1rem;
+
+	> li {
+		margin: 0.2rem 1rem;
+	}
+
+	@media (max-width: 768px) {
+		padding: 0;
+		width: 100%;
+		justify-content: center;
+	}
+`;
+
+export default List;
