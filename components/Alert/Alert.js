607342eed30725e052a05c034a9788a1c5496d86
Erick Sarabia <ecksarabia@gmail.com>
2021-03-01 15:23:33 -0600

    Add Aler component to display error messages

---
+++ components/Alert/Alert.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const Alert = styled.span`
+	position: fixed;
+	bottom: 0;
+	color: #fff;
+	margin-top: 1rem;
+	font-size: 1rem;
+	font-size: 1rem;
+	width: 100%;
+	padding: 0.2rem;
+	text-align: center;
+	background: #e56475;
+	padding: 1rem;
+`;
+
+export default Alert;
