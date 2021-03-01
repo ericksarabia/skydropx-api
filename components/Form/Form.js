021ec3ba3168c50233eee6e1f03785cb6f821a0d
Erick Sarabia <ecksarabia@gmail.com>
2021-03-01 17:26:55 -0600

    Fix missing keys

--- components/Form/Form.js
+++ components/Form/Form.js
@@ -1,14 +1,15 @@
-import { Wrapper, Input, Button, Label, Group, Message } from "./styled";
+import Alert from "../Alert";
+import { Wrapper, Input, Button, Label, Group } from "./styled";
 
 const Form = ({ handleClick, inputs, message }) => (
 	<Wrapper>
 		{inputs.map((input) => (
-			<Group>
+			<Group key={input.label}>
 				{input.label && <Label>{input.label}</Label>}
 				<Input type={input.type} placeholder={input.placeholder} id={input.id} />
 			</Group>
 		))}
-		{message && <Message>Todos los campos son obligatorios</Message>}
+		{message && <Alert>ERROR: Todos los campos son obligatorios</Alert>}
 		<Button onClick={handleClick}>Aceptar</Button>
 	</Wrapper>
 );
