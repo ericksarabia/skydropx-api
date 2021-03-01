63f65e13d660eb0f7781e6460c12140dc0a45cd9
Erick Sarabia <ecksarabia@gmail.com>
2021-03-01 17:26:37 -0600

    Add support for width, padding and border props

--- components/Container/Container.js
+++ components/Container/Container.js
@@ -1,12 +1,15 @@
 import styled from "styled-components";
 
 const Container = styled.div`
-	width: 100%;
+	width: "100%";
+	padding: ${(props) => props.padding || "0"};
 	min-height: 100vh;
 	display: flex;
 	justify-content: center;
 	align-items: center;
 	flex-direction: column;
+	text-align: center;
+	border: ${(props) => (props.border ? "5px solid #4e34e1" : "0")};
 `;
 
 export default Container;
