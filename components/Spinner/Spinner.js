07a9b5fcfeda1c516c3b5a36bddc074e32fdb65f
Erick Sarabia <ecksarabia@gmail.com>
2021-03-01 21:42:56 -0600

    Add Spinnercomponent for fetch

---
+++ components/Spinner/Spinner.js
@@ -0,0 +1,9 @@
+import { Wrapper, Loader } from "./styled";
+
+const Spinner = () => (
+	<Wrapper>
+		<Loader />
+	</Wrapper>
+);
+
+export default Spinner;
---
+++ components/Spinner/styled.js
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+	position: absolute;
+	top: 0;
+	background: rgba(255, 255, 255, 0.9);
+	width: 100%;
+	height: 100vh;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`;
+
+const Loader = styled.div`
+	border: 4px solid rgba(0, 0, 0, 0.1);
+	width: 36px;
+	height: 36px;
+	border-radius: 50%;
+	border-left-color: #4e34e1;
+	animation: spin 1s ease infinite;
+
+	@keyframes spin {
+		0% {
+			transform: rotate(0deg);
+		}
+
+		100% {
+			transform: rotate(360deg);
+		}
+	}
+`;
+
+export { Wrapper, Loader };
